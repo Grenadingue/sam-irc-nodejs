17d25f95188da2c52a63be3cd11cbe5e84c427da
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-02-06 12:31:47 +0530

    Quick and dirty update of html GUI + Instanciate sequelize with mariadb

--- server/server.js
+++ server/server.js
@@ -1,3 +1,36 @@
-#!/usr/bin/env node
+#!/usr/bin/env nodemon
+
+var express = require("express")
+var app = express()
+
+var Sequelize = require("sequelize")
+var sequelize = new Sequelize("sam_irc", "archy", "bar", {
+  host: "localhost",
+  dialect: "mariadb",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+})
 
 console.log("Hello world ! Server")
+
+app.use(express.static(__dirname + "/www/html/"))
+app.use(express.static(__dirname + "/www/css/"))
+
+app.post("/signup", function (req, res) {
+  console.log("signup")
+  // res.send("Sign-up POST request")
+  res.redirect("http://localhost:8080/signin.html")
+});
+
+app.post("/signin", function (req, res) {
+  console.log("signin")
+  // res.send("Sign-in POST request")
+  res.redirect("http://localhost:8080/index.html") // redirect to irc
+})
+
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+})
