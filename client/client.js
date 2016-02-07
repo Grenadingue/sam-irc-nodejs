881b7fc280b7f41fab9882d7a8316a12a8a9ad90
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-02-07 09:44:52 +0530

    Update code to make jshint and linter-jscs happy

--- client/client.js
+++ client/client.js
@@ -1,32 +1,30 @@
 #!/usr/bin/env electron
 
-'use strict'
-
-const electron = require("electron")
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-app.on("window-all-closed", function() {
-  app.quit()
-})
+app.on('window-all-closed', function () {
+  app.quit();
+});
 
-app.on("ready", function() {
+app.on('ready', function () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     center: true,
-  })
+  });
 
-  mainWindow.loadURL("http://localhost:8080/index.html")
+  mainWindow.loadURL('http://localhost:8080/index.html');
 
-  mainWindow.on("closed", function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  })
-})
+  });
+});
--- server/server.js
+++ server/server.js
@@ -1,36 +1,38 @@
 #!/usr/bin/env nodemon
 
-var express = require("express")
-var app = express()
+var express = require('express');
+var app = express();
 
-var Sequelize = require("sequelize")
-var sequelize = new Sequelize("sam_irc", "archy", "bar", {
-  host: "localhost",
-  dialect: "mariadb",
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('sam_irc', 'archy', 'bar', {
+  host: 'localhost',
+  dialect: 'mariadb',
   pool: {
     max: 5,
     min: 0,
-    idle: 10000
-  }
-})
-
-console.log("Hello world ! Server")
-
-app.use(express.static(__dirname + "/www/html/"))
-app.use(express.static(__dirname + "/www/css/"))
-
-app.post("/signup", function (req, res) {
-  console.log("signup")
-  // res.send("Sign-up POST request")
-  res.redirect("http://localhost:8080/signin.html")
+    idle: 10000,
+  },
 });
 
-app.post("/signin", function (req, res) {
-  console.log("signin")
-  // res.send("Sign-in POST request")
-  res.redirect("http://localhost:8080/index.html") // redirect to irc
-})
+console.log('Hello world ! Server');
+
+app.use(express.static(__dirname + '/www/html/'));
+app.use(express.static(__dirname + '/www/css/'));
+
+app.post('/signup', function (req, res) {
+  console.log('signup');
+
+  // res.send('Sign-up POST request');
+  res.redirect('http://localhost:8080/signin.html');
+});
+
+app.post('/signin', function (req, res) {
+  console.log('signin');
+
+  // res.send('Sign-in POST request');
+  res.redirect('http://localhost:8080/index.html'); // redirect to irc
+});
 
 app.listen(8080, function () {
-  console.log("Example app listening on port 8080!");
-})
+  console.log('Example app listening on port 8080!');
+});
